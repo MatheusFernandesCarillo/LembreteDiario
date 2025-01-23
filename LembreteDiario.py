fbcc4fb3e63f1e4c746a40cfa61cd4ba32404e09
MatheusFernandesCarillo <75450635+MatheusFernandesCarillo@users.noreply.github.com>
2025-01-23 16:48:13 -0300

    adicionado comentarios

--- LembreteDiario.py
+++ LembreteDiario.py
@@ -2,7 +2,7 @@ import schedule
 import time
 from plyer import notification
 
-def enviar_lembrete():
+def enviar_lembrete(): #definir a função enviar_lembrete
     notification.notify(
         title="Lembrete Água",
         message="Bebe Agua",
@@ -11,8 +11,11 @@ def enviar_lembrete():
     )
 
 
-schedule.every(1).hour.do(enviar_lembrete)
+schedule.every(1).hour.do(enviar_lembrete) #fazer com que a notificação aparece a cada uma hora
 
-while True:
+while True: #loop para manter o script rodando
     schedule.run_pending()
     time.sleep(1) 
+
+# utilizado pyinstaller para transformar o arquivo .py em .exe
+# pyinstaller --onefile --noconsole LembreteDiario.py
